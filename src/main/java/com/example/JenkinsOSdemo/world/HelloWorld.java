e976729c3cd755ca250ff16e058b96cfcc0c0b93
KRK7917 <kkellogg@woodmen.org>
2017-08-23 10:59:23 -0500

    adding sample hello world and corresponding simple test

---
+++ src/main/java/com/example/JenkinsOSdemo/world/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.JenkinsOSdemo.world;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorld {
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
