640a758e904d9916886fd6364b4363747f289499
KRK7917 <kkellogg@woodmen.org>
2017-08-23 08:54:00 -0500

    Initial commit

---
+++ src/main/java/com/example/JenkinsOSdemo/JenkinsOSdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.JenkinsOSdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsOSdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsOSdemoApplication.class, args);
+	}
+}
